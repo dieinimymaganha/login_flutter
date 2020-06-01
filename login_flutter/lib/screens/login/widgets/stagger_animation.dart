d93bd18f4da3edefb6f4664bf3e82f1a3ae8e87d
Dieinimy <dieinimy@gmail.com>
2020-06-01 20:17:21 -0300

    Finalizando a animação do botão

--- login_flutter/lib/screens/login/widgets/stagger_animation.dart
+++ login_flutter/lib/screens/login/widgets/stagger_animation.dart
@@ -3,10 +3,18 @@ import 'package:flutter/material.dart';
 class StaggerAnimation extends StatelessWidget {
   final AnimationController controller;
   final Animation<double> buttonSqueeze;
+  final Animation<double> buttonZoomOut;
 
   StaggerAnimation({this.controller})
       : buttonSqueeze = Tween(begin: 320.0, end: 60.0).animate(
-            CurvedAnimation(parent: controller, curve: Interval(0.0, 0.150)));
+            CurvedAnimation(parent: controller, curve: Interval(0.0, 0.150))),
+        buttonZoomOut = Tween(
+          begin: 60.0,
+          end: 1000.0,
+        ).animate(CurvedAnimation(
+          parent: controller,
+          curve: Interval(0.5, 1, curve: Curves.bounceOut),
+        ));
 
   @override
   Widget build(BuildContext context) {
@@ -23,21 +31,32 @@ class StaggerAnimation extends StatelessWidget {
         onTap: () {
           controller.forward();
         },
-        child: Container(
-            width: buttonSqueeze.value,
-            height: 60,
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              color: Color.fromRGBO(247, 64, 106, 1.0),
-              borderRadius: BorderRadius.all(Radius.circular(30.0)),
-            ),
-            child: _buildInside(context)),
+        child: buttonZoomOut.value <= 60
+            ? Container(
+                width: buttonSqueeze.value,
+                height: 60,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(247, 64, 106, 1.0),
+                  borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                ),
+                child: _buildInside(context))
+            : Container(
+                width: buttonZoomOut.value,
+                height: buttonZoomOut.value,
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(247, 64, 106, 1.0),
+                  shape: buttonZoomOut.value < 500
+                      ? BoxShape.circle
+                      : BoxShape.rectangle,
+                ),
+              ),
       ),
     );
   }
 
   Widget _buildInside(BuildContext context) {
-    if(buttonSqueeze.value > 75) {
+    if (buttonSqueeze.value > 75) {
       return Text(
         'Sign in',
         style: TextStyle(
@@ -46,7 +65,7 @@ class StaggerAnimation extends StatelessWidget {
             fontSize: 20,
             letterSpacing: 0.3),
       );
-    }else{
+    } else {
       return CircularProgressIndicator(
         valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
         strokeWidth: 1.0,
